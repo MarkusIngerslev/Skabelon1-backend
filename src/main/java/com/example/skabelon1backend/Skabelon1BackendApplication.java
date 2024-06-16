6e3640e8ef5af81c07b1b13e3757e53e114c715d
MarkusIngerslev <markusolsen2004@gmail.com>
2024-06-17 00:19:58 +0200

    Skabelon Projekt init

---
+++ src/main/java/com/example/skabelon1backend/Skabelon1BackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.skabelon1backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Skabelon1BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Skabelon1BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/skabelon1backend/Skabelon1BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skabelon1backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Skabelon1BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
